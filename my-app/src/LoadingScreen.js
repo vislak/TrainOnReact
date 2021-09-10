97780061f12badb81a088ecd251b1c4af9505776
Vis <Vis@LAPTOP-7UL3FSUK>
2021-09-11 04:45:32 +0530

    added empty componenent LoadingScreen

---
+++ my-app/src/LoadingScreen.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class LoadingScreen extends Component
+{
+
+
+
+    render()
+    {
+      return <div>
+
+            Loading....
+
+      </div>  
+    }
+}
